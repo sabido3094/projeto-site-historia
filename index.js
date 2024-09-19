9278958b48b61316329200433c4764e3026e4f45
sabido3094 <00001094802207sp@al.educacao.sp.gov.br>
2024-09-19 17:53:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const buttons = document.querySelectorAll('.btn-proximo');
-    const jumpscare = document.getElementById('jumpscare');
-    const jumpscareSound = document.getElementById('jumpscare-sound');
+const avanca = document.querySelectorAll('.btn-proximo');
 
-    buttons.forEach(button => {
-        button.addEventListener('click', (event) => {
-            const currentActive = document.querySelector('.ativo');
-            const nextStep = event.target.getAttribute('data-proximo');
-            const nextStepElement = document.getElementById(passo-${nextStep});
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-            // Navegar para o próximo passo
-            if (currentActive) {
-                currentActive.classList.remove('ativo');
-            }
-            if (nextStepElement) {
-                nextStepElement.classList.add('ativo');
-            }
-
-            // Mostrar jumpscare se o próximo passo for 4
-            if (nextStep === '4') {
-                jumpscare.classList.remove('hidden');
-                jumpscareSound.play();
-            }
-        });
-    });
-});
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
