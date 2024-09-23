19dd435d127e0ddc4baf617f7cef46f4ad44bbef
sabido3094 <00001094802207sp@al.educacao.sp.gov.br>
2024-09-23 16:48:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
-    button.addEventListener('click', function(){
+    button.addEventListener('click', function() {
         const atual = document.querySelector('.ativo');
         const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
+       
+        if (proximoPasso === 'passo-7') {
+            const imagem = document.querySelector('#passo-7 img');
+         
+            imagem.classList.add('fade');
+           
+            setTimeout(() => {
+                imagem.classList.add('fade-in'); 
+            }, 0); 
+        }
+
         atual.classList.remove('ativo');
         document.getElementById(proximoPasso).classList.add('ativo');
-    })
-})
+    });
+});
